c9a2cfa4ada460b0f621a65bde5f6f9b8d4d0a04
TheGroobi <23wilk.szymon@gmail.com>
2026-07-18 14:21:01 +0200

    feat: enable humidity and parse weathe

--- firmware/weather_module/main/weather_module.c
+++ firmware/weather_module/main/weather_module.c
@@ -7,14 +7,55 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 #include <freertos/task.h>
+#include <inttypes.h>
 #include <stdint.h>
+#include <sys/types.h>
 
 #define SDA_PIN 6
 #define SCL_PIN 7
 #define BIT_LENGTH 8
 
-#define DEVICE_ADDRESS 0x76 // ESP32-S3 address
+#define DEV_ADDR 0x76 // Sensor address
 #define CTRL_MEAS_ADDR 0xF4
+#define TEMP_ADDR 0xF7
+#define CTR_HUM_ADDR 0xF2
+#define WHO_AM_I_ADDR 0x60
+
+typedef struct {
+  uint32_t pressure;
+  uint32_t temperature;
+  uint16_t humidity;
+} bme280_data_t;
+
+// weather order starting from temp_addr
+// 24 bits of temperature
+// 20 bits of pressure (need to right shift the padding)
+// 16 bits of humidity
+
+uint32_t concat_bytes(uint8_t *bytes, int start, size_t len) {
+  uint32_t value = 0;
+
+  for (int i = start; i < start + len; i++) {
+    value = ((value << 8) | bytes[i]);
+  };
+
+  return value;
+}
+
+int read_who_am_i(i2c_master_dev_handle_t dev_handle) {
+  uint8_t buf;
+  uint8_t err;
+
+  err = i2c_master_receive(dev_handle, &buf, 1, 1000);
+  printf("BME280 ID: %u\n", buf);
+  return err;
+}
+
+int enable_humidity(i2c_master_dev_handle_t dev_handle) {
+  uint8_t osrs_h = 6; // in binary: 00000111
+  uint8_t config[] = {CTR_HUM_ADDR, osrs_h};
+  return i2c_master_transmit(dev_handle, config, 2, 1000);
+}
 
 int set_config(i2c_master_dev_handle_t dev_handle) {
   uint8_t osrs_p = 1;
@@ -23,23 +64,41 @@ int set_config(i2c_master_dev_handle_t dev_handle) {
   uint8_t value = ((osrs_t << 5) | (osrs_p << 2) | mode);
   uint8_t config[] = {CTRL_MEAS_ADDR, value};
 
+  uint8_t err;
+  err = enable_humidity(dev_handle);
+  if (err != 0)
+    return err;
+
   return i2c_master_transmit(dev_handle, config, 2, 1000);
 }
 
+bme280_data_t parse_weather(uint8_t *buf) {
+  bme280_data_t data;
+
+  // right shift by 4 to avoid padding
+  data.pressure = concat_bytes(buf, 0, 3) >> 4;
+  data.temperature = concat_bytes(buf, 3, 3) >> 4;
+  data.humidity = concat_bytes(buf, 6, 2);
+
+  return data;
+}
+
 int talk(i2c_master_dev_handle_t dev_handle) {
-  uint8_t reg = CTRL_MEAS_ADDR;
-  uint8_t buf[BIT_LENGTH];
+  uint8_t reg = TEMP_ADDR;
+  uint8_t buf[BIT_LENGTH] = {0};
   int err;
 
   err = i2c_master_transmit_receive(dev_handle, &reg, 1, buf, BIT_LENGTH, 1000);
 
-  for (int i = 0; i < BIT_LENGTH; i++) {
-    printf("reg 0x%02X = 0x%02X\n", reg + i, buf[i]);
-  }
+  bme280_data_t weather = parse_weather(buf);
+
+  // compensation for human readable format
+  printf("pressure raw: %" PRIu32 "\n", weather.pressure);
+  printf("temp raw: %" PRIu32 "\n", weather.temperature);
+  printf("humidity raw: %u\n", weather.humidity);
 
   return err;
 }
-
 void app_main(void) {
 
   i2c_master_bus_config_t i2c_mst_config = {
@@ -56,26 +115,35 @@ void app_main(void) {
 
   i2c_device_config_t dev_cfg = {
       .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-      .device_address = DEVICE_ADDRESS,
+      .device_address = DEV_ADDR,
       .scl_speed_hz = 100000,
   };
 
   i2c_master_dev_handle_t dev_handle;
   ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
 
-  // look for the address of the pin
-  // for (int addr = 1; addr < 127; addr++) {
-  //   printf("skipped0x%02X\n", addr);
-  //   esp_err_t res = i2c_master_probe(bus_handle, addr, 100);
-  //   if (res == ESP_OK) {
-  //     printf("Found device at: 0x%02X\n", addr);
-  //   }
-  // }
-  //
+  ESP_ERROR_CHECK(read_who_am_i(dev_handle));
 
-  set_config(dev_handle);
-  while (1) {
-    ESP_ERROR_CHECK(talk(dev_handle));
+  // look for the address of the pin
+  for (int addr = 1; addr < 127; addr++) {
+    esp_err_t res = i2c_master_probe(bus_handle, addr, 100);
+    if (res == ESP_OK) {
+      printf("Found device at: 0x%02X\n", addr);
+    }
+  }
+
+  ESP_ERROR_CHECK(set_config(dev_handle));
+  vTaskDelay(100);
+
+  ESP_ERROR_CHECK(enable_humidity(dev_handle));
+  vTaskDelay(100);
+
+  for (;;) {
+    esp_err_t err = talk(dev_handle);
+
+    if (err != ESP_OK) {
+      printf("I2C error: %s\n", esp_err_to_name(err));
+    }
     vTaskDelay(1000);
   };
 }
